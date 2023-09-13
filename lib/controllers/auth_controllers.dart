64b71e987e4d8fe40241b7084826c993cc7a5df0
Ike Odira <odiraike8@gmail.com>
2023-09-13 02:41:31 -0400

    Add Image Picker

--- lib/controllers/auth_controllers.dart
+++ lib/controllers/auth_controllers.dart
@@ -1,9 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AuthController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Function Select image from gallery or camera
+
+  pickProfileImage(ImageSource source) async {
+    final ImagePicker _imagePicker = ImagePicker();
+    XFile? _file = await _imagePicker.pickImage(source: source);
+    if (_file != null) {
+      return _file.readAsBytes;
+    } else {
+      print("No Image Selected or Captured");
+    }
+  }
 
   Future<String> createNewUser(
       String email, String fullName, String password) async {
